3a97baa632c573f213680d37ac04d94f7ea977ef
Himanshu <41159246+Himanshu9802@users.noreply.github.com>
2023-05-23 11:04:27 +0530

    Update CircularProgress.jsx

--- src/components/reusables/CircularProgress/CircularProgress.jsx
+++ src/components/reusables/CircularProgress/CircularProgress.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+// import React from "react";
+// import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+// import "react-circular-progressbar/dist/styles.css";
 
-const CircularProgress = (props) => {
-  const percent = props.percent
-  const language = props.language
-  const icon = props.icon
+// const CircularProgress = (props) => {
+//   const percent = props.percent
+//   const language = props.language
+//   const icon = props.icon
 
-  return (
-    <div>
-      <div className="progress p90">
-        <CircularProgressbar
-          value={percent}
-          text={`${percent}%`}
-          styles={buildStyles({
-            pathColor: `rgb(255, 210, 76)`,
-            trailColor: "#585d65",
-          })}
-        />
-        <div className="percentage"></div>
-        <span>{`${percent}%`}</span>
-      </div>
-      <div className="name" style={{ marginLeft: "0px" }}>
-        {language}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div className="progress p90">
+//         <CircularProgressbar
+//           value={percent}
+//           text={`${percent}%`}
+//           styles={buildStyles({
+//             pathColor: `rgb(255, 210, 76)`,
+//             trailColor: "#585d65",
+//           })}
+//         />
+//         <div className="percentage"></div>
+//         <span>{`${percent}%`}</span>
+//       </div>
+//       <div className="name" style={{ marginLeft: "0px" }}>
+//         {language}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CircularProgress;
+// export default CircularProgress;
